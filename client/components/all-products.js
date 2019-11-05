928d1bee42d9f0be1b1abd160c902cbbf0aedb88
Peet Klecha <p.a.klecha@gmail.com>
2019-11-05 16:02:19 -0500

    small-product component done

---
+++ client/components/all-products.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {getAllProducts} from '../store/product'
+import {SmallProduct} from './small-product'
---
+++ client/components/small-product.js
@@ -0,0 +1,18 @@
+/*eslint-disable react/display-name*/
+import React from 'react'
+import {Card, Icon, Image} from 'semantic-ui-react'
+
+export default props => (
+  <Card>
+    <Image src={props.imageUrl} />
+    <Card.Content>
+      <Card.Header>{props.name}</Card.Header>
+      {/* <Card.Meta>MANUFACTURER GOES HERE</Card.Meta> */}
+      <Card.Description>{props.description}</Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <Icon name="dollar sign" />
+      {props.price}
+    </Card.Content>
+  </Card>
+)
