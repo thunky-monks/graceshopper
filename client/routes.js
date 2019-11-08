8b649c99de43c0e46e172c1502791d9c36e311b5
Erin Fung <erinfungg@gmail.com>
2019-11-08 15:20:24 -0500

    add guest components

--- client/routes.js
+++ client/routes.js
@@ -2,10 +2,18 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, AllProducts, BigProduct} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  AllProducts,
+  BigProduct,
+  CartView,
+  GuestAllProducts,
+  GuestBigProduct,
+  GuestCartView
+} from './components'
 import {me} from './store'
-import CartView from './components/cart-view'
-import {getCart} from './store/cart'
 
 import {getAllProducts} from './store/product'
 
@@ -25,11 +33,24 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route exact path="/products" component={AllProducts} />
-        <Route path="/products/:id" component={BigProduct} />
-        <Route path="/cart" component={CartView} />
+        {/* <Route exact path="/products" component={AllProducts} />
+        <Route path="/products/:id" component={BigProduct} /> */}
+        {isLoggedIn ? (
+          <Switch>
+            <Route exact path="/products" component={AllProducts} />
+            <Route path="/products/:id" component={BigProduct} />
+            <Route path="/cart" component={CartView} />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route exact path="/products" component={GuestAllProducts} />
+            <Route path="/products/:id" component={GuestBigProduct} />
+            <Route path="/cart" component={GuestCartView} />
+          </Switch>
+        )}
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
