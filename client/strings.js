3b67dff1a97fbce67bcb81520ea4bd8a8c6ee030
Joshua Grossman <Josh1794@gmail.com>
2019-11-12 13:26:35 -0500

    order-history-price

--- client/strings.js
+++ client/strings.js
@@ -3,4 +3,9 @@ export const CART_HEADER = 'Shopping Cart'
 export const WEBSITE_HEADER = 'E-Commerce-Commerce'
 export const WEBSITE_SUB_HEADER = 'An E-Commerce-Commerce Company'
 export const ORDER_HISTORY_PANEL_LABEL = order =>
-  `Order #7 (${order.datePurchased}) Total: $7.32`
+  `Date Purchased: (${order.datePurchased}) Total: $${order.products.reduce(
+    (sum, product) => {
+      return sum + product.product_cart.priceAtPurchase
+    },
+    0
+  )}`
