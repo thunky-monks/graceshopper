cf00ad62176e83d1c1391331f4cb7ba84760e24e
Peet Klecha <p.a.klecha@gmail.com>
2019-11-12 16:13:15 -0500

    editing-profile

--- client/store/user.js
+++ client/store/user.js
@@ -1,35 +1,48 @@
-import axios from 'axios'
-import history from '../history'
+import axios from 'axios';
+import history from '../history';
 
 /**
  * ACTION TYPES
  */
-export const GET_USER = 'GET_USER'
-const REMOVE_USER = 'REMOVE_USER'
+export const GET_USER = 'GET_USER';
+const REMOVE_USER = 'REMOVE_USER';
+const EDITED_USER = 'EDITED_USER';
 
 /**
  * INITIAL STATE
  */
-const defaultUser = {}
+const defaultUser = {};
 
 /**
  * ACTION CREATORS
  */
-const getUser = (user, cart) => ({type: GET_USER, user, cart})
-const removeUser = () => ({type: REMOVE_USER})
+const getUser = (user, cart) => ({ type: GET_USER, user, cart });
+const removeUser = () => ({ type: REMOVE_USER });
+const editedUser = user => ({ type: EDITED_USER, user });
 
 /**
  * THUNK CREATORS
  */
+
+export const editUser = user => async dispatch => {
+  try {
+    const userRes = await axios.put(`/api/users/${user.id}/`, user);
+    if (userRes) dispatch(editedUser(userRes.data));
+    else console.log('error udpating user profile');
+  } catch (error) {
+    console.log('error updating user profile');
+  }
+};
+
 export const me = () => async dispatch => {
   try {
-    const userRes = await axios.get('/auth/me')
-    const cartRes = await axios.get(`/api/users/${userRes.data.id}/cart`)
-    dispatch(getUser(userRes.data || defaultUser, cartRes.data))
+    const userRes = await axios.get('/auth/me');
+    const cartRes = await axios.get(`/api/users/${userRes.data.id}/cart`);
+    dispatch(getUser(userRes.data || defaultUser, cartRes.data));
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 export const auth = (
   firstName,
@@ -38,40 +51,40 @@ export const auth = (
   password,
   method
 ) => async dispatch => {
-  let userRes
+  let userRes;
   try {
     userRes = await axios.post(`/auth/${method}`, {
       email,
       password,
       firstName,
       lastName
-    })
+    });
   } catch (authError) {
-    return dispatch(getUser({error: authError}))
+    return dispatch(getUser({ error: authError }));
   }
 
   try {
     if (method === 'signup') {
-      dispatch(getUser(userRes.data, []))
+      dispatch(getUser(userRes.data, []));
     } else {
-      const cartRes = await axios.get(`/api/users/${userRes.data.id}/cart`)
-      dispatch(getUser(userRes.data, cartRes.data))
+      const cartRes = await axios.get(`/api/users/${userRes.data.id}/cart`);
+      dispatch(getUser(userRes.data, cartRes.data));
     }
-    history.push('/home')
+    history.push('/home');
   } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
+    console.error(dispatchOrHistoryErr);
   }
-}
+};
 
 export const logout = () => async dispatch => {
   try {
-    await axios.post('/auth/logout')
-    dispatch(removeUser())
-    history.push('/login')
+    await axios.post('/auth/logout');
+    dispatch(removeUser());
+    history.push('/login');
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 /**
  * REDUCER
@@ -79,10 +92,10 @@ export const logout = () => async dispatch => {
 export default function(state = defaultUser, action) {
   switch (action.type) {
     case GET_USER:
-      return action.user
+      return action.user;
     case REMOVE_USER:
-      return defaultUser
+      return defaultUser;
     default:
-      return state
+      return state;
   }
 }
